86c9faa4854fee1d011b9ca8a320215c46e4c3ce
alvaroo611 <alvaromarmolromero@gmail.com>
2023-10-24 00:39:57 +0200

    Correcion clase PokemonCardGame

--- pokemon/src/main/java/es/iesjandula/graphicInterface/PokemonCardGame.java
+++ pokemon/src/main/java/es/iesjandula/graphicInterface/PokemonCardGame.java
@@ -1,23 +1,20 @@
 package es.iesjandula.graphicInterface;
-
 import javax.swing.*;
-
 import es.iesjandula.pokemon.utils.Pokemon;
-
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.List;
 
-public class  PokemonCardGame extends JFrame {
+public class PokemonCardGame extends JFrame {
     private List<Pokemon> player1Deck;
     private List<Pokemon> player2Deck;
     private int player1CardIndex = 0;
     private int player2CardIndex = 0;
 
-    public  PokemonCardGame(List<Pokemon> player1Deck, List<Pokemon> player2Deck) {
-    	
+    private JPanel player1Panel;
+    private JPanel player2Panel;
+    private CardLayout cardLayout;
+
+    public PokemonCardGame(List<Pokemon> player1Deck, List<Pokemon> player2Deck) {
         this.player1Deck = player1Deck;
         this.player2Deck = player2Deck;
 
@@ -26,50 +23,64 @@ public class  PokemonCardGame extends JFrame {
         setResizable(false);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        JPanel player1Panel = new JPanel();
-        JPanel player2Panel = new JPanel();
+        player1Panel = new JPanel();
+        player2Panel = new JPanel();
+        cardLayout = new CardLayout();
+        player1Panel.setLayout(cardLayout);
+        player2Panel.setLayout(cardLayout);
 
         add(player1Panel, BorderLayout.WEST);
         add(player2Panel, BorderLayout.EAST);
 
         JButton player1NextButton = new JButton("Siguiente Jugador 1");
+        JButton player1PrevButton = new JButton("Anterior Jugador 1");
         JButton player2NextButton = new JButton("Siguiente Jugador 2");
-
-        player1Panel.setLayout(new BorderLayout());
-        player2Panel.setLayout(new BorderLayout());
+        JButton player2PrevButton = new JButton("Anterior Jugador 2");
 
         player1NextButton.addActionListener(e -> {
-            player1CardIndex = (player1CardIndex + 1) ;
-            player1Panel.removeAll();
-            player1Panel.add(new CardPanel(player1Deck.get(player1CardIndex)));
-            player1Panel.revalidate();
-            player1Panel.repaint();
+            player1CardIndex = (player1CardIndex + 1) % player1Deck.size();
+            cardLayout.show(player1Panel, Integer.toString(player1CardIndex));
+        });
+
+        player1PrevButton.addActionListener(e -> {
+            player1CardIndex = (player1CardIndex - 1 + player1Deck.size()) % player1Deck.size();
+            cardLayout.show(player1Panel, Integer.toString(player1CardIndex));
         });
 
         player2NextButton.addActionListener(e -> {
             player2CardIndex = (player2CardIndex + 1) % player2Deck.size();
-            player2Panel.removeAll();
-            player2Panel.add(new CardPanel(player2Deck.get(player2CardIndex)));
-            player2Panel.revalidate();
-            player2Panel.repaint();
+            cardLayout.show(player2Panel, Integer.toString(player2CardIndex));
         });
 
-        player1Panel.add(player1NextButton, BorderLayout.SOUTH);
-        player2Panel.add(player2NextButton, BorderLayout.SOUTH);
-        
+        player2PrevButton.addActionListener(e -> {
+            player2CardIndex = (player2CardIndex - 1 + player2Deck.size()) % player2Deck.size();
+            cardLayout.show(player2Panel, Integer.toString(player2CardIndex));
+        });
+
+        JPanel buttonPanel1 = new JPanel();
+        JPanel buttonPanel2 = new JPanel();
+        buttonPanel1.add(player1PrevButton);
+        buttonPanel1.add(player1NextButton);
+        buttonPanel2.add(player2PrevButton);
+        buttonPanel2.add(player2NextButton);
+
+        add(buttonPanel1, BorderLayout.SOUTH);
+        add(buttonPanel2, BorderLayout.SOUTH);
+
         displayInitialCards(player1Panel, player2Panel);
-    	
     }
 
     private void displayInitialCards(JPanel player1Panel, JPanel player2Panel) {
-        player1Panel.add(new CardPanel(player1Deck.get(player1CardIndex)));
-        player2Panel.add(new CardPanel(player2Deck.get(player2CardIndex)));
+        for (int i = 0; i < player1Deck.size(); i++) {
+            player1Panel.add(new CardPanel(player1Deck.get(i)), Integer.toString(i));
+        }
+        for (int i = 0; i < player2Deck.size(); i++) {
+            player2Panel.add(new CardPanel(player2Deck.get(i)), Integer.toString(i));
+        }
+        cardLayout.show(player1Panel, Integer.toString(player1CardIndex));
+        cardLayout.show(player2Panel, Integer.toString(player2CardIndex));
     }
-
-   
 }
-
-
 
 class CardPanel extends JPanel {
     private Pokemon card;
@@ -79,6 +90,11 @@ class CardPanel extends JPanel {
         setPreferredSize(new Dimension(200, 300));
     }
 
+    public void updateCard(Pokemon card) {
+        this.card = card;
+        repaint();
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
@@ -86,7 +102,6 @@ class CardPanel extends JPanel {
         g.fillRect(0, 0, getWidth(), getHeight());
         g.setColor(Color.BLACK);
         g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
-        // Dibuja la representación de la carta en el panel, puedes usar el método toString de la clase Pokemon
         String pokemonInfo = card.toString();
         g.drawString(pokemonInfo, 10, 30);
     }
