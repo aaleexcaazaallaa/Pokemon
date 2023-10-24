0dc9947d8ec35b80570630821aab077ceae6e6d6
alvaroo611 <alvaromarmolromero@gmail.com>
2023-10-24 12:47:28 +0200

    Implantacion de imagenes

--- pokemon/src/main/java/es/iesjandula/graphicInterface/CardPanel.java
+++ pokemon/src/main/java/es/iesjandula/graphicInterface/CardPanel.java
@@ -3,14 +3,21 @@ package es.iesjandula.graphicInterface;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import es.iesjandula.pokemon.utils.Pokemon;
 
 public class CardPanel extends JPanel {
     private Pokemon card;
-
+    private JLabel imagenLabel;
     public CardPanel(Pokemon card) {
         this.card = card;
         setPreferredSize(new Dimension(200, 300));
@@ -39,5 +46,23 @@ public class CardPanel extends JPanel {
         g.drawString("Sp.Defense: " +String.valueOf(card.getSpDefense()), 10, 190);
         g.drawString("Generation: " +String.valueOf(card.getGeneration()), 10, 210);
         g.drawString("Legendary: " +String.valueOf(card.isLegendary()), 10, 230);
+        try {
+        	// Dentro del bloque try-catch donde cargas la imagen
+        	BufferedImage imagen = ImageIO.read(new File("imagen.png"));
+
+        	// Especifica el nuevo ancho y alto deseado para la imagen reescalada
+        	int nuevoAncho = 100; // Ajusta el valor a tu preferencia
+        	int nuevoAlto = 100;  // Ajusta el valor a tu preferencia
+
+        	// Reescala la imagen
+        	Image imagenReescalada = imagen.getScaledInstance(nuevoAncho, nuevoAlto, Image.SCALE_SMOOTH);
+
+        	// Configura el JLabel para mostrar la imagen reescalada
+        	g.drawImage(imagenReescalada, 10, 250, this);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
     }
 }
